a32882e5d5382d21863ef86c43f03389d2c904b5
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2023-10-18 16:20:39 +0300

    Hello service version 1.0

---
+++ src/main/java/ru/diasoft/edu/controller/HelloController.java
@@ -0,0 +1,12 @@
+package ru.diasoft.edu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello from service!";
+    }
+}
